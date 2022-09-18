b603e9f430ebd6ce78c8319972c2e9dbfa31a9d1
Anastasia <super.okay335@yandex.ru>
2022-09-18 23:07:59 +0300

    Добавили решение 1 задачи

--- example_001/Program.cs
+++ example_001/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+Console.Write ("Введите положительное значение M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите положительное значение N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if ((M < 0) || (N < 0) )
+{ 
+    Console.WriteLine ("Вы ввели не натуральные числа");
+}
+if ((N>M) && ((M>0) && (N>0)))
+for (int i = M; i <= N; i++)
+    Console.Write($" {i}");
+else
+if ((M>0) && (N>0))
+    for (int i = N; i <= M; i++)
+        Console.Write($" {i}");
+
