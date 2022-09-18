147b0a3aa2c63a4fb047ffb59308dbd6e0a959a0
Anastasia <super.okay335@yandex.ru>
2022-09-18 23:20:59 +0300

    Добавили решение 2 задачи

--- example_002/Program.cs
+++ example_002/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.Write ("Введите положительное значение M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите положительное значение N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int Sum = 0;
+if ((M < 0) || (N < 0) )
+{ 
+    Console.WriteLine ("Вы ввели не натуральные числа");
+}
+if ((N>M) && ((M>0) && (N>0)))
+for (int i = M; i <= N; i++)
+    Sum  = Sum + M++;
+else
+if ((M>0) && (N>0))
+    for (int i = N; i <= M; i++)
+        Sum  = Sum + N++;
+Console.WriteLine ($"Сумма натуральных элементов в промежутке от M до N: {Sum}");
